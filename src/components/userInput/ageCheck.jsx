1198c923c85ce9b0a64a273db435ba2a46413e43
Eemeli Surakka <eemeli.surakka@hotmail.fi>
2020-08-01 12:12:45 +0200

    age check repaired and ready

--- src/components/userInput/ageCheck.jsx
+++ src/components/userInput/ageCheck.jsx
@@ -1,24 +1,24 @@
 import React, { useState } from 'react';
 
 function ageCheck() {
-
   const [ name, setname ] = useState('');
   const [ age, setAge] = useState('');
+  const [ ageResponse, setAgeResponse ] = useState('');
 
-  const ageCheck = () => age < 18;
+  const ageCheck = (arg) => arg < 18;
 
-  const appendToPage = () => ageCheck(age)
+  const ageMessage = (arg) => ageCheck(arg)
     ? 'You are too young.'
     : `Hello ${name}!`
 
   return (
     <div className="App">
-      <p className='greeting'></p>
+      <p className='greeting'>{ageResponse}</p>
       <label>Name: </label>
       <input type='text' name='name' onChange={(e) => {setname(e.target.value)}}/>
       <label>age: </label>
       <input type='number' name='age' onChange={(e) => {setAge(e.target.value)}}/>
-      <button onClick={() => console.log(appendToPage())}>Submit</button>
+      <button onClick={() => setAgeResponse(ageMessage(age))}>Submit</button>
     </div>
   );
 }
